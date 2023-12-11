17834ff630f0f6fcf449157e3bbe2e0f77737c97
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2023-12-11 22:17:06 +0100

    tgtg

--- create_star_schema.py
+++ create_star_schema.py
@@ -65,7 +65,7 @@ queries = [init_q, create_date_dim_q, create_location_dim_q, create_school_dim_q
 # Database connection details
 db_name = "student_sport_academics_DW"
 username = "postgres"
-password = "postgres"
+password = "lina2015"
 
 try:
     # Establish database connection
