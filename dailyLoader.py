26468d4ea5c0d7d718a8486d3cc1edf750fcbf36
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2023-12-13 18:06:37 +0100

    new

---
+++ dailyLoader.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import sqlalchemy
+import random
+from datetime import datetime, timedelta
+from ETL_pipeline import loader_data, sqldf
+
+# Function to generate surrogate keys
+def generate_surrogate_keys(start, end, count):
+    return random.sample(range(start, end), count)
+
+# Function to get the latest timestamp from a table
+def get_latest_timestamp(table, column, engine):
+    query = f"SELECT MAX({column}) FROM {table};"
+    result = pd.read_sql(query, engine)
+    return result.iloc[0, 0]  # Assuming a single result
+
+# Function to add new rows to a dimension table
+def insert_new_rows(engine, table, df, surrogate_key_col, timestamp_col):
+    latest_timestamp = get_latest_timestamp(table, timestamp_col, engine)
+    
+    if pd.isnull(latest_timestamp):
+        latest_timestamp = datetime(1970, 1, 1)  # A default date in the past
+
+    new_rows = df[df[timestamp_col] > latest_timestamp]
+    
+    if not new_rows.empty:
+        new_rows[surrogate_key_col] = generate_surrogate_keys(10000, 99999, len(new_rows))
+        new_rows.to_sql(name=table, con=engine, schema="student_ath", if_exists="append", index=False)
+        print(f"Inserted {len(new_rows)} new rows into {table}.")
+    else:
+        print(f"No new rows to insert into {table}.")
+
+# Function to load new data from CSV files
+def load_new_data():
+    # Load existing dataframes using ETL_pipeline
+    sap_red_df, school_location_df, contact_sports_df = loader_data()
+
+    # Load CSV files with new data (replace 'your_file_path' with the actual file path)
+    new_sap_red_df = pd.read_csv('NCAA_school_academic_performance_new_data.csv')
+    new_school_location_df = pd.read_csv('List_of_NCAA_Division_I_institutions_new_data.csv')
+    new_contact_sports_df = pd.read_csv('contact_sports_new_data.csv')
+
+    return sap_red_df, school_location_df, contact_sports_df, new_sap_red_df, new_school_location_df, new_contact_sports_df
+
+# Load existing dataframes using ETL_pipeline
+sap_red_df, school_location_df, contact_sports_df = loader_data()
+
+# Load new data from CSV files
+new_sap_red_df, new_school_location_df, new_contact_sports_df = load_new_data()
+
+# Create or connect to the PostgreSQL database engine
+engine = sqlalchemy.create_engine("postgresql://postgres:postgres@localhost/student_sport_academics_DW")
+
+# Insert new rows into the date dimension
+insert_new_rows(engine, "date_dim", new_sap_red_df[['ACADEMIC_YEAR']].drop_duplicates(), 'date_key', 'your_timestamp_column')
+
+# Insert new rows into the location dimension
+insert_new_rows(engine, "location_dim", new_school_location_df[['School', 'Common Name', 'Nickname', 'City', 'State', 'Type', 'Subdivision', 'Primary Conference']].drop_duplicates(), 'location_key', 'your_timestamp_column')
+
+# Insert new rows into the school dimension
+insert_new_rows(engine, "school_dim", new_sap_red_df[['SCHOOL_NAME']].drop_duplicates(), 'school_key', 'your_timestamp_column')
+
+# Insert new rows into the sport dimension
+insert_new_rows(engine, "sport_dim", new_sap_red_df[['SPORT_NAME']].drop_duplicates(), 'sport_key', 'your_timestamp_column')
+
+# Insert new rows into the fact table
+insert_new_rows(engine, "academic_score_snapshot_fact", new_sap_red_df, ['SCHOOL_ID', 'ACADEMIC_YEAR', 'SPORT_CODE'], 'your_timestamp_column')
+
+# Note: Replace 'your_timestamp_column' with the actual timestamp column in your dataframes
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,93 @@
-connection
+# loader.py
+import random
+import sqlalchemy
+import ETL_pipeline
+import pandas as pd
+
+# Initialize counter and generate surrogate keys
+i = 0
+surrogate_key_list = random.sample(range(10000, 99999), 1000)
+
+# Load dataframes using ETL_pipeline
+sap_red_df, school_location_df, contact_sports_df = ETL_pipeline.loader_data()
+
+# Create date dimension
+date_dim = pd.DataFrame(sap_red_df['year'].drop_duplicates().reset_index(drop=True))
+date_dim['date_key'] = 0
+
+# Assign surrogate keys to date dimension
+date_dim['date_key'] = surrogate_key_list[i:i+len(date_dim)]
+i += len(date_dim)
+
+# Create location dimension
+location_dim = pd.DataFrame(school_location_df[['school_name', 'school_conference', 'school_type']].drop_duplicates().reset_index(drop=True))
+location_dim['location_key'] = 0
+
+# Assign surrogate keys to location dimension
+location_dim['location_key'] = surrogate_key_list[i:i+len(location_dim)]
+i += len(location_dim)
+
+# Create school dimension
+school_dim = pd.DataFrame(sap_red_df['school_name'].drop_duplicates().reset_index(drop=True))
+school_dim['school_key'] = 0
+
+# Assign surrogate keys to school dimension
+school_dim['school_key'] = surrogate_key_list[i:i+len(school_dim)]
+i += len(school_dim)
+
+# Create sport dimension
+sport_dim = pd.DataFrame(sap_red_df[['gender', 'sport']].drop_duplicates().reset_index(drop=True))
+sport_dim['sport_key'] = 0
+
+# Assign surrogate keys to sport dimension
+sport_dim['sport_key'] = surrogate_key_list[i:i+len(sport_dim)]
+i += len(sport_dim)
+
+# Query to join school_dim and school_location_df using pandas merge
+# school_dim = pd.merge(school_dim, school_location_df[['school_name', 'school_conference', 'school_type']], on='school_name', how='inner')
+school_dim = pd.merge(school_dim, school_location_df, on='school_name', how='inner')
+
+# Query to join sport_dim and contact_sports_df
+sport_dim_q = '''
+    SELECT sp.sport_key,
+            sp.gender,
+            sp.sport,
+            c.contact_sport
+    FROM sport_dim sp
+    INNER JOIN contact_sports_df c
+    ON sp.sport = c.sport;
+'''
+sport_dim = ETL_pipeline.sqldf(sport_dim_q, locals())
+
+# Create fact table from dimension tables and original raw table
+fact_tbl_query = '''
+    SELECT d.date_key,
+           l.location_key,
+           sc.school_key,
+           sp.sport_key,
+           st.num_athletes,
+           st.academic_score
+    FROM sap_red_df st
+    INNER JOIN date_dim d ON st.year = d.year
+    INNER JOIN location_dim l ON st.school_name = l.school_name
+    INNER JOIN school_dim sc ON st.school_name = sc.school_name
+    INNER JOIN sport_dim sp ON st.gender = sp.gender AND st.sport = sp.sport;
+'''
+
+academic_score_snapshot_fact = ETL_pipeline.sqldf(fact_tbl_query, locals())
+
+# Drop 'school_name' column from location_dim
+location_dim = location_dim.drop('school_name', axis=1)
+
+# Loading dimension/fact tables into PostgreSQL database
+engine = sqlalchemy.create_engine("postgresql://postgres:postgres@localhost/student_sport_academics_DW")
+
+# Define the tables variable outside the loop
+tables = [date_dim, location_dim, school_dim, sport_dim, academic_score_snapshot_fact]
+table_names = ["date_dim", "location_dim", "school_dim", "sport_dim", "academic_score_snapshot_fact"]
+
+# Insert data into PostgreSQL tables
+for i in range(len(tables)):
+    tables[i].to_sql(name=table_names[i], con=engine, schema="student_ath", if_exists="append", index=False)
+
+print("\nData successfully loaded into the star schema.")
