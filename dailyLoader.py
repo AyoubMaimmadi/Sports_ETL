0a9166e0f2478d1eebd72a4591ea919dfdcbd329
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2023-12-16 21:29:30 +0100

    new

--- dailyLoader.py
+++ dailyLoader.py
@@ -1,18 +1,9 @@
 
 import pandas as pd
-import sqlalchemy
 from sqlalchemy import create_engine
-import os
-
-# Database connection details (to be filled with actual credentials)
-DB_USERNAME = 'your_username'
-DB_PASSWORD = 'your_password'
-DB_HOST = 'your_host'
-DB_PORT = 'your_port'
-DB_NAME = 'your_database'
 
 # Creating a database engine
-engine = create_engine(f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+engine = create_engine("postgresql://postgres:postgres@localhost/student_sport_academics_DW")
 
 # Function to read data from a CSV file
 def read_csv(file_path):
@@ -23,10 +14,16 @@ def read_db_table(table_name):
     return pd.read_sql_table(table_name, engine)
 
 # Function to find new records
-def find_new_records(existing_data, new_data):
-    # Assuming that there is a unique identifier to compare. This will need to be adjusted based on actual data structure
-    existing_ids = set(existing_data['unique_id'])
-    new_data['is_new'] = new_data['unique_id'].apply(lambda x: x not in existing_ids)
+def find_new_records(existing_data, new_data, unique_columns):
+    # Creating a set of tuples from the existing data for comparison
+    existing_ids = set(tuple(row) for row in existing_data[unique_columns].to_records(index=False))
+    
+    # Function to check if a record in new data is new
+    def is_new_record(row):
+        return tuple(row[unique_columns].values) not in existing_ids
+    
+    # Applying the function to filter new records
+    new_data['is_new'] = new_data.apply(is_new_record, axis=1)
     return new_data[new_data['is_new']]
 
 # Main function to load data
@@ -40,5 +37,16 @@ def load_data(csv_file_path, db_table_name):
     else:
         print('No new records to insert.')
 
-# Example usage (to be adjusted for actual file paths and table names)
-# load_data('/path/to/your/csv_file.csv', 'your_table_name')
+# Mapping of CSV file paths to database table names
+csv_to_table_mapping = {
+    'List_of_NCAA_Division_I_institutions.csv': 'school_dim',
+    'contact_sports.csv': 'sport_dim',
+    'List_of_NCAA_Division_I_institutions.csv': 'location_dim', 
+    'NCAA_school_academic_performance.csv': 'date_dim',
+    'NCAA_school_academic_performance.csv': 'academic_score_snapshot_fact'
+}
+
+# Looping over the mapping and loading data for each table
+for csv_file, table_name in csv_to_table_mapping.items():
+    load_data(csv_file, table_name)
+
